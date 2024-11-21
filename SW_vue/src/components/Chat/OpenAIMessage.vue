dff980c43e162c768557f032c01e140c6bb8c9dd
SunjunSS <zinzza122@naver.com>
2024-11-22 00:51:41 +0900

    OpenAIMessage.vue 수정

--- SW_vue/src/components/Chat/OpenAIMessage.vue
+++ SW_vue/src/components/Chat/OpenAIMessage.vue
@@ -2,7 +2,8 @@
   <div class="ai-message">
     <img class="profile-image" :src="profileImage" alt="AI" />
     <div class="message-text">
-      <span v-for="(part, index) in parsedMessage" :key="index">
+      <template v-for="(part, index) in parsedMessage" :key="index">
+        <br v-if="part.isNewline" />
         <a 
           v-if="part.isBookTitle" 
           href="#" 
@@ -12,7 +13,7 @@
           {{ part.text }}
         </a>
         <span v-else>{{ part.text }}</span>
-      </span>
+      </template>
     </div>
   </div>
 </template>
@@ -38,35 +39,72 @@ export default {
     parsedMessage() {
       const text = this.message.text;
       const parts = [];
-      let currentIndex = 0;
-
-      // <<책제목>> 패턴을 찾아서 분리
-      const regex = /<<([^>]+)>>/g;
+      
+      // 새로운 로직으로 메시지 파싱
+      const regex = /<<([^>]+)>>|(\d+\.)|\n\n/g;
+      let lastIndex = 0;
       let match;
 
       while ((match = regex.exec(text)) !== null) {
-        // 책 제목 앞의 일반 텍스트 추가
-        if (match.index > currentIndex) {
+        // 이전 텍스트 추가
+        if (match.index > lastIndex) {
           parts.push({
-            text: text.substring(currentIndex, match.index),
-            isBookTitle: false
+            text: text.substring(lastIndex, match.index),
+            isBookTitle: false,
+            isNewline: false
           });
         }
 
-        // 책 제목 추가
-        parts.push({
-          text: match[1], // << >> 기호를 제외한 실제 책 제목
-          isBookTitle: true
-        });
+        // 숫자로 시작하는 부분 처리 (새 줄 추가)
+        if (match[2]) {
+          // 첫 번째 숫자일 때는 개행을 추가하지 않음
+          if (parts.length === 0 || parts[parts.length - 1].isNewline) {
+            parts.push({
+              text: match[0],
+              isBookTitle: false,
+              isNewline: false // 처음 숫자는 개행 없이 추가
+            });
+          } else {
+            parts.push({
+              text: match[0],
+              isBookTitle: false,
+              isNewline: true // 이후 숫자부터는 개행 추가
+            });
+          }
+        }
 
-        currentIndex = match.index + match[0].length;
+        // 책 제목 처리
+        if (match[1]) {
+          parts.push({
+            text: match[1],
+            isBookTitle: true,
+            isNewline: false
+          });
+        }
+
+        // 이중 개행 처리
+        if (match[0] === '\n\n') {
+          parts.push({
+            text: '',
+            isBookTitle: false,
+            isNewline: true
+          });
+          parts.push({
+            text: '',
+            isBookTitle: false,
+            isNewline: true
+          });
+        }
+
+        lastIndex = match.index + match[0].length;
       }
 
       // 마지막 남은 텍스트 추가
-      if (currentIndex < text.length) {
+      if (lastIndex < text.length) {
         parts.push({
-          text: text.substring(currentIndex),
-          isBookTitle: false
+          text: text.substring(lastIndex),
+          isBookTitle: false,
+          isNewline: false
         });
       }
 
